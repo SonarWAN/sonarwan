f350b503887f7b56459d3d5e194038217c443eda
Ivan Itzcovich <i.itzcovich@gmail.com>
2016-07-01 00:34:25 -0300

    Add stream classes

--- sonarwan.py
+++ sonarwan.py
@@ -5,18 +5,62 @@ def show_progress(pkg_index):
     sys.stdout.write("\rProcessed packets {}".format(pkg_index))
     sys.stdout.flush()
 
+def create_http_stream(pkg):
+        if hasattr(pkg.http, 'request'):
+            return HTTPStream(pkg.tcp.stream,
+                                    ip_src = pkg.ip.src, ip_dst = pkg.ip.dst,
+                                    port_src = pkg.tcp.srcport, port_dst = pkg.tcp.dstport)
+        return HTTPStream(pkg.tcp.stream,
+                                ip_src = pkg.ip.dst, ip_dst = pkg.ip.src,
+                                port_src = pkg.tcp.dstport, port_dst = pkg.tcp.srcport)
+
+class Stream(object):
+
+    def __init__(self, number, **kwargs):
+        self.number = number
+        self.ip_src = kwargs['ip_src']
+        self.ip_dst = kwargs['ip_dst']
+        self.port_src = kwargs['port_src']
+        self.port_dst = kwargs['port_dst']
+
+    def __repr__(self):
+        return ' -> '.join(['({} - {})'.format(self.ip_src, self.port_src),
+                            '({} - {})'.format(self.ip_dst, self.port_dst)])
+
+
+class TCPStream(Stream):
+
+    def __init__(self, number, **kwargs):
+        super().__init__(number, **kwargs)
+
+
+class HTTPStream(TCPStream):
+
+    def __init__(self, number, **kwargs):
+        super().__init__(number, **kwargs)
+
+
 class Device(object):
 
     def __init__(self):
         self.name = 'Unknown device'
         self.model = None
-        self.streams = set()
+        self.streams = [] # List of Streams
 
     def __repr__(self):
         return '<Device: {}>'.format(str(self))
 
     def __str__(self):
         return self.name
+
+    def add_stream(self, stream):
+        self.streams.append(stream)
+
+    def __contains__(self, stream):
+        for s in self.streams:
+            if s.number == stream.number:
+                return True
+        return False
 
 
 class Environment(object):
@@ -28,16 +72,23 @@ class Environment(object):
         app_layer = pkg.layers[-1]
 
         if app_layer.layer_name == 'http' and hasattr(pkg, 'tcp'):
+            stream = create_http_stream(pkg)
             for d in self.devices:
-                if pkg.tcp.stream in d.streams:
+                if stream in d:
                     # update scenario
                     return
             device = Device()
             if hasattr(app_layer, 'user_agent'):
                 device.name = app_layer.user_agent
                 device.model = app_layer.user_agent.split(',')[0]
-            device.streams.add(pkg.tcp.stream)
+            device.add_stream(stream)
             self.devices.append(device)
+
+    def pretty_print(self):
+        for d in self.devices:
+            print('Device: {}'.format(d))
+            for s in d.streams:
+                print('\tStream {}: {}'.format(s.number, s))
 
 
 class SonarWan(object):
@@ -53,7 +104,7 @@ class SonarWan(object):
             i+=1
             show_progress(i)
             protocol = pkg.layers[-1].layer_name
-            if protocol in ['http', 'dns']:
+            if protocol in ['http']:
                 env.update(pkg)
         print()
 
@@ -63,4 +114,4 @@ if __name__ == '__main__':
     reader = SonarWan(environment=env)
     reader.analyze(sys.argv[1])
 
-    print(env.devices)
+    env.pretty_print()
