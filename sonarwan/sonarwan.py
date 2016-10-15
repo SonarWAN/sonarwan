1cb4a17351d911cb7d1c6d89dd9c8b6b1fbfc613
Ivan Itzcovich <i.itzcovich@gmail.com>
2016-10-15 16:01:07 -0300

    Add argparse

--- sonarwan/sonarwan.py
+++ sonarwan/sonarwan.py
@@ -1,6 +1,8 @@
 import pyshark
 import sys
 import time
+import argparse
+import json
 
 from environment import Environment
 
@@ -9,13 +11,18 @@ import utils
 from tools import main_tools
 
 
+
 class SonarWan(object):
-    def __init__(self):
+    def __init__(self, json_output):
 
         self.environment = Environment(
             ua_analyzer=main_tools.UserAgentAnalyzer(),
             inference_engine=main_tools.InferenceEngine())
         self.i = 0
+
+        self.json_output = json_output
+
+        self.start_time = time.time()
 
     def analyze(self, path):
         cap = pyshark.FileCapture(path)
@@ -23,19 +30,36 @@ class SonarWan(object):
 
         for pkg in cap:
             self.i += 1
-            utils.show_progress(self.i)
+            if not self.json_output:
+                utils.show_progress(self.i)
             self.environment.update(pkg)
-        print()
 
-    def pretty_print(self):
-        self.environment.pretty_print()
+    def print_info(self):
+        if self.json_output:
+            pass
+        else:
+            self.environment.pretty_print()
+            print('Execution time: {}'.format((time.time() - self.start_time)))
+
+
+def make_argparse():
+    parser = argparse.ArgumentParser(
+            description="Recognize devices of a private network by sniffing NAT'd traffic",
+            epilog="For suggestions or bug report, go to https://github.com/sonarwan/sonarwan-core")
+    parser.add_argument('files', nargs='+', help='list of capture files')
+    parser.add_argument("-j", "--json", help="outputs information in JSON format.", action="store_true")
+    return parser
+
+def main():
+    args = make_argparse().parse_args()
+
+    sonarwan = SonarWan(args.json)
+
+    for each in args.files:
+        sonarwan.analyze(each)
+
+    sonarwan.print_info()
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    sonarwan = SonarWan()
-    for arg in sys.argv[1:]:
-        sonarwan.analyze(arg)
-
-    sonarwan.pretty_print()
-    print('Execution time: {}'.format((time.time() - start_time)))
+    main()
