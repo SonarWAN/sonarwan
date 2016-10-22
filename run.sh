7bc5f4636cf871f279fd246d6f89005af525f10f
Federico Bond <federicobond@gmail.com>
2016-10-22 15:25:06 -0300

    Fix files with spaces

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # cd to the script directory
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit 1
 
 source env/bin/activate
-python sonarwan/sonarwan.py --json $*
+python sonarwan/sonarwan.py --json "$@"
