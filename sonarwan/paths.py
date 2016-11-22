2548859ea95df22d60592ac748c6f473e5b65d2f
Federico Bond <federicobond@gmail.com>
2016-11-22 14:18:18 -0300

    Make paths more resilient

--- sonarwan/paths.py
+++ sonarwan/paths.py
@@ -1,9 +1,13 @@
-USER_AGENT_PATTERNS_FILE = './db/user_agents.patterns'
+import os
 
-INFERENCE_DIR = './db/inference/'
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-MOBILE_DETECTOR_PATH = './db/serbanghita-Mobile-Detect-db.json'
+USER_AGENT_PATTERNS_FILE = os.path.join(BASE_DIR, './db/user_agents.patterns')
 
-SERVICES_DIRECTORY_PATH = './db/services/'
+INFERENCE_DIR = os.path.join(BASE_DIR, './db/inference/')
 
-LINUX_DISTRIBUTION_FILE = './db/linux.distributions'
+MOBILE_DETECTOR_PATH = os.path.join(BASE_DIR, './db/serbanghita-Mobile-Detect-db.json')
+
+SERVICES_DIRECTORY_PATH = os.path.join(BASE_DIR, './db/services/')
+
+LINUX_DISTRIBUTION_FILE = os.path.join(BASE_DIR, './db/linux.distributions')
